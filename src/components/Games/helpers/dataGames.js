cfb1978ed83c764b892b0f3096baada657eea14a
Diego Valencia <ve18007@ues.edu.sv>
2023-07-09 11:16:33 -0600

    unidad 2 terminada

--- src/components/Games/helpers/dataGames.js
+++ src/components/Games/helpers/dataGames.js
@@ -3,13 +3,19 @@ import img2 from "../../../assets/SVG/DrawKit Vector Illustration Influencer & C
 import img3 from "../../../assets/SVG/DrawKit Vector Illustration Influencer & Content Creator Illustrations (3).svg";
 import img4 from "../../../assets/SVG/DrawKit Vector Illustration Influencer & Content Creator Illustrations (5).svg";
 
-import imgG1 from "../../../assets/SVG2/drawkit-grape-pack-illustration-1.svg";
-import imgG2 from "../../../assets/SVG2/drawkit-grape-pack-illustration-8.svg";
-import imgG3 from "../../../assets/SVG2/drawkit-grape-pack-illustration-11.svg";
-import imgG4 from "../../../assets/SVG2/drawkit-grape-pack-illustration-10.svg";
-import imgG5 from "../../../assets/SVG2/drawkit-grape-pack-illustration-16.svg";
-import imgG6 from "../../../assets/SVG2/drawkit-grape-pack-illustration-17.svg";
-import imgG7 from "../../../assets/SVG2/drawkit-grape-pack-illustration-18.svg";
+import imgU1_1 from "../../../assets/SVG2/drawkit-grape-pack-illustration-1.svg";
+import imgU1_2 from "../../../assets/SVG2/drawkit-grape-pack-illustration-8.svg";
+import imgU1_3 from "../../../assets/SVG2/drawkit-grape-pack-illustration-11.svg";
+import imgU1_4 from "../../../assets/SVG2/drawkit-grape-pack-illustration-10.svg";
+import imgU1_5 from "../../../assets/SVG2/drawkit-grape-pack-illustration-16.svg";
+import imgU1_6 from "../../../assets/SVG2/drawkit-grape-pack-illustration-17.svg";
+import imgU1_7 from "../../../assets/SVG2/drawkit-grape-pack-illustration-18.svg";
+
+import imgU2_1 from "../../../assets/SVG3/wfh_1.svg";
+import imgU2_2 from "../../../assets/SVG3/wfh_8.svg";
+import imgU2_3 from "../../../assets/SVG3/wfh_10.svg";
+import imgU2_4 from "../../../assets/SVG3/wfh_5.svg";
+import imgU2_5 from "../../../assets/SVG3/wfh_7.svg";
 
 export const dataGames = [
   {
@@ -22,49 +28,49 @@ export const dataGames = [
         id_game: 10,
         title: "El diálogo: lenguaje estándar.",
         description: "Descubre las noticias más importantes de la semana",
-        image: imgG1,
+        image: imgU1_1,
         link: "https://wordwall.net/es/resource/56981083",
       },
       {
         id_game: 11,
         title: "El restaurante: diálogo informal.",
         description: "Descubre las noticias más importantes de la semana",
-        image: imgG2,
+        image: imgU1_2,
         link: "https://wordwall.net/es/resource/27921868/hiszpa%c5%84ski/en-el-restaurante-dialogo",
       },
       {
         id_game: 12,
         title: "El plural de las palabras terminadas en -z.",
         description: "Descubre las noticias más importantes de la semana",
-        image: imgG3,
+        image: imgU1_3,
         link: "https://wordwall.net/es/resource/56979574",
       },
       {
         id_game: 14,
         title: "El uso del punto.",
         description: "Descubre las noticias más importantes de la semana",
-        image: imgG4,
+        image: imgU1_4,
         link: "https://wordwall.net/es/resource/56980484",
       },
       {
         id_game: 15,
         title: "Verbos en la narración.",
         description: "Descubre las noticias más importantes de la semana",
-        image: imgG5,
+        image: imgU1_5,
         link: "https://wordwall.net/es/resource/6438448/verbos-en-la-narraci%C3%B3n",
       },
       {
         id_game: 16,
         title: "Preguntas que responde la noticia.",
         description: "Descubre las noticias más importantes de la semana",
-        image: imgG7,
+        image: imgU1_6,
         link: "https://wordwall.net/es/resource/38271401/la-noticia",
       },
       {
         id_game: 17,
         title: "Temas de la noticia.",
         description: "Descubre las noticias más importantes de la semana",
-        image: imgG6,
+        image: imgU1_7,
         link: "https://wordwall.net/es/resource/34743528/temas-de-la-noticia",
       },
     ],
@@ -78,10 +84,45 @@ export const dataGames = [
     games: [
       {
         id_game: 20,
-        title: "El plural de las palabras terminadas en -z.",
-        description: "Descubre las noticias más importantes de la semana",
-        image: imgG3,
-        link: "https://wordwall.net/es/resource/56979574",
+        title: "La narración de aventuras maravillosas.",
+        description: "Descubre mundos sin límites a través de la lectura",
+        image: imgU2_1,
+        link: "https://wordwall.net/es/resource/58383946",
+      },
+      {
+        id_game: 21,
+        title: "Estructura de la narración de aventuras",
+        description: "Descubre los hilos que tejen una trama cautivadora",
+        image: imgU2_2,
+        link: "https://wordwall.net/resource/58384141",
+      },
+      {
+        id_game: 22,
+        title: "Los pronombres personales.",
+        description: "Palabras mágicas que nos conectan con otros.",
+        image: imgU2_3,
+        link: "https://wordwall.net/resource/58384141",
+      },
+      {
+        id_game: 23,
+        title: "Párrafos en Acción: Juego de Redacción 1",
+        description: "¡Encuentra la palabra perdida! Juegos de párrafos para armar.",
+        image: imgU2_4,
+        link: "https://wordwall.net/resource/58384380",
+      },
+      {
+        id_game: 24,
+        title: "Párrafos en Acción: Juego de Redacción 2",
+        description: "¡Encuentra la palabra perdida! Juegos de párrafos para armar.",
+        image: imgU2_5,
+        link: "https://wordwall.net/resource/37659014/flor-ar-parrafo",
+      },
+      {
+        id_game: 25,
+        title: "Párrafos en Acción: Juego de Redacción 2",
+        description: "¡Encuentra la palabra perdida! Juegos de párrafos para armar.",
+        image: imgU2_5,
+        link: "https://wordwall.net/resource/37659014/flor-ar-parrafo",
       },
     ],
     image: img2,
@@ -96,14 +137,14 @@ export const dataGames = [
         id_game: 21,
         title: "El uso del punto.",
         description: "Descubre las noticias más importantes de la semana",
-        image: imgG4,
+        image: imgU1_4,
         link: "https://wordwall.net/es/resource/56980484",
       },
       {
         id_game: 22,
         title: "Verbos en la narración.",
         description: "Descubre las noticias más importantes de la semana",
-        image: imgG5,
+        image: imgU1_5,
         link: "https://wordwall.net/es/resource/6438448/verbos-en-la-narraci%C3%B3n",
       },
     ],
@@ -119,14 +160,14 @@ export const dataGames = [
         id_game: 23,
         title: "Preguntas que responde la noticia.",
         description: "Descubre las noticias más importantes de la semana",
-        image: imgG7,
+        image: imgU1_7,
         link: "https://wordwall.net/es/resource/38271401/la-noticia",
       },
       {
         id_game: 24,
         title: "Temas de la noticia.",
         description: "Descubre las noticias más importantes de la semana",
-        image: imgG6,
+        image: imgU1_7,
         link: "https://wordwall.net/es/resource/34743528/temas-de-la-noticia",
       },
     ],
--- src/components/Home/AnimationHome.jsx
+++ src/components/Home/AnimationHome.jsx
@@ -1,4 +1,4 @@
-import { Player, Controls } from '@lottiefiles/react-lottie-player';
+import { Player, Controls } from "@lottiefiles/react-lottie-player";
 export const AnimationHome = () => {
   return (
     <Player
@@ -6,7 +6,10 @@ export const AnimationHome = () => {
       loop
       src="https://assets8.lottiefiles.com/packages/lf20_sknsem5j.json"
     >
-      <Controls visible={false} buttons={["play", "repeat", "frame", "debug"]} />
+      <Controls
+        visible={false}
+        buttons={["play", "repeat", "frame", "debug"]}
+      />
     </Player>
   );
 };
